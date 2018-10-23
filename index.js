0ac82c0414eee792baacfc04e0fddcecd8261490
paulo4713 <paulo4713@hotmail.com>
2018-10-22 21:20:42 -0500

    mongo añadido, aun no funciona la base de datos

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars')
+var mongo = require('mongodb');
+var assert = require('assert');
 
+//MongoClient.connect("mongodb://localhost:27017/tayle-web", { useNewUrlParser: true });
 //Asociamos express a la app
 const app = express();
 
@@ -23,7 +26,6 @@ app.get('/', function(request, response){
     //para llamar algun archivo se debe usar path.join(__dirname, 'ruta del archivo')
         //var ruta = path.join(__dirname, 'public/index.html');
         var contexto = {
-            
         };
     response.render('index', contexto);
 });
@@ -59,7 +61,24 @@ app.get('/optica', function(request, response){
           ]
     };
     response.render('optica', contexto);
+    /*
+    var resultArray = [];
+    mongo.connect(url, function(err, db){
+        assert.equal(null, err);
+        var cursor = db.collection('productos').find();
+        cursor.forEach(function(doc, err){
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function () {
+            db.close;   
+            res.render('optica', {items: resultArray});
+        });
+    });
+    */
+
+  
 });
+
 
 //RUTA DINÁMICA
 //Al usar /: estoy usando una variable
